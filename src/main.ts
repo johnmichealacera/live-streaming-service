569fd5f5c5bc233b755c0a350b6aade2d4e13c53
John Micheal Acera <john.acera@sportality.se>
2025-04-16 19:50:06 +0800

    add env variable for LIVE_APP_URL

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // TODO: add it in env variable
+    origin: process.env.LIVE_APP_URL,
   });
   await app.listen(process.env.PORT ?? 3001);
 }
